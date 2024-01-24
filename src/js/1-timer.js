82a714c93f5a2de24c4839a5c5d1af61924947f5
lenor4ik <leno4kamay08051987@gmail.com>
2024-01-24 18:28:08 +0100

    ff

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -11,9 +11,4 @@ const options = {
   },
 };
 
-<script>
-  flatpickr("#datetime-picker", {
-    dateFormat: "d.m.Y", // Формат дати
-    locale: "ru", // Якщо ви хочете використовувати російську мову, замість "ru" вкажіть код потрібної мови
-  });
-</script>
+
